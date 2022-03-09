792bd1ba3f271723fde32c6948e80238886ac604
James Walker <james.chunho+github@gmail.com>
2022-03-09 17:00:07 +0000

    Add styling to create a phone at the center of the screen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import "./app.css"
 
 class App extends Component {
   render() {
     return (
-      <div>TEST</div>
-    )
+      <div className='phone-container'>
+        <div className='phone'>
+          TEST
+        </div>
+      </div>
+    );
   }
 }
 
-export default App
+export default App;
