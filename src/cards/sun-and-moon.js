99faea072a474037dba459757fe3c1bf3f7ada9c
d-arde <dardeleanu00@gmail.com>
2022-03-09 18:26:20 +0000

    added part of Sun and Moon class

--- src/cards/sun-and-moon.js
+++ src/cards/sun-and-moon.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
-import './common.css';
+import './sun-moon.css';
 
 class SunAndMoon extends Component {
   render() {
     return (
       <div className="card">
+        <p>Sun and Moon</p>
+        <p></p>
       </div>
     );
   }
