f6a780a962ce4d9072256f0a81b05af7be3535bb
d-arde <dardeleanu00@gmail.com>
2022-03-13 11:49:19 +0000

    update img to show properly

--- src/cards/sun-and-moon.js
+++ src/cards/sun-and-moon.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './sun-moon.css';
+import imagePath from './sun-moon-images/solar.jpeg'
 
 class SunAndMoon extends Component {
   render() {
     return (
       <div className="card">
         <p>Sun and Moon</p>
-        <img src="./sun-moon-images/solar.jpeg" alt="solar graph"></img>
+        <img src={imagePath} alt="solar graph"></img>
       </div>
     );
   }
