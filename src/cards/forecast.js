a91d49e65ae7486cdf6e2d295d8fd49c505e4c1c
James Walker <james.chunho+github@gmail.com>
2022-03-15 19:14:00 +0000

    WIP on Forecast card

--- src/cards/forecast.js
+++ src/cards/forecast.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 
 import './common.css';
+import './forecast.css';
+import circleSvgPath from "../assets/circle-info-solid.svg";
 
 class Forecast extends Component {
   render() {
     return (
-      <div className="card">
-        ASIDHGSAJKHDAGSKD
+      <div className="card forecast">
+        <div className='meta-info'>
+          <div className='icon'>
+            <svg width="18" height="18">
+              <image href={circleSvgPath} width="18" height="18" />
+            </svg>
+          </div>
+          Updated at 17:15
+        </div>
+        <h2>Forecast</h2>
+        <div className='graphs'>
+          sajghd sajghd sajghd sajghd sajghd sajghd sajghd sajghd sajghd sajghd sajghd sajghd sajghd sajghd sajghd sajghd sajghd sajghd sajghd sajghd sajghd sajghd sajghd sajghd sajghd sajghd sajghd sajghd sajghd<br/>
+          sajghd<br/>
+          sajghd<br/>
+          sajghd<br/>
+          sajghd<br/>
+          sajghd<br/>
+
+        </div>
       </div>
     );
   }
