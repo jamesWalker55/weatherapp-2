b8270b80e48d543cd2ad6e47dd9ff22dc4f62a88
James Walker <james.chunho+github@gmail.com>
2022-03-17 00:40:43 +0000

    Use HourlyChart in Forecast card

--- src/cards/forecast.js
+++ src/cards/forecast.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import './common.css';
 import './forecast.css';
 import circleSvgPath from "../assets/circle-info-solid.svg";
-import HourlyChart from './parts/hourly-chart'
+import HourlyChart from './parts/hourly-chart';
 
 class Forecast extends Component {
   render() {
@@ -18,15 +18,15 @@ class Forecast extends Component {
           Updated at 17:15
         </div>
         <h2>Forecast</h2>
-        <div className='graphs'>
-          <HourlyChart apiData={this.props.apiData} type="line" source="temp" />
-          qwerty qwerty qwerty qwerty qwerty qwerty qwerty qwerty qwerty qwerty qwerty qwerty qwerty qwerty qwerty qwerty qwerty qwerty qwerty qwerty qwerty qwerty qwerty qwerty qwerty qwerty qwerty qwerty qwerty<br />
-          qwerty<br />
-          qwerty<br />
-          qwerty<br />
-          qwerty<br />
-          qwerty<br />
-
+        <div className='graphs-container' style={{ position: 'relative' /* for graph axis positioning */ }}>
+          <div className='graphs-scroller' style={{ overflowX: "scroll" }}>
+            <div className='graphs' style={{ width: "60rem" }}>
+              <HourlyChart apiData={this.props.apiData} title="Temperature" source="temp" unit="celsius" />
+              <HourlyChart apiData={this.props.apiData} title="Humidity" source="humidity" unit="percent" />
+              <HourlyChart apiData={this.props.apiData} title="Clouds" source="clouds" unit="percent" />
+              <HourlyChart apiData={this.props.apiData} title="Pressure" source="pressure" unit="hPa" showDates={true} />
+            </div>
+          </div>
         </div>
       </div>
     );
