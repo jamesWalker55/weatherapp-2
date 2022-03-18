8106939d7239b4b1d76b777bf1ddd4a8ae9a0d41
James Walker <james.chunho+github@gmail.com>
2022-03-18 00:40:14 +0000

    Use longer description for header

--- src/components/header.js
+++ src/components/header.js
@@ -4,11 +4,16 @@ import './header.css';
 
 class Header extends Component {
   render() {
+    let statusText = this.props.apiData.current.weather[0].description;
+
+    // capitalize the first letter
+    statusText = statusText[0].toUpperCase() + statusText.slice(1);
+
     return (
       <div className="header">
         <div className="location">Mile End Rd (E1 4NS)</div>
         <div className="temperature">{Math.round(this.props.apiData.current.temp - 273.15)}°</div>
-        <div className="status-text">{this.props.apiData.current.weather[0].main}</div>
+        <div className="status-text">{statusText}</div>
       </div>
     );
   }
