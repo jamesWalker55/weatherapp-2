2b41c7068fa94ce47298588336efe7f3614aa1b5
James Walker <james.chunho+github@gmail.com>
2022-03-18 00:13:56 +0000

    Improve star map card

--- src/cards/star-map.js
+++ src/cards/star-map.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 
 import './common.css';
+import './star-map.css';
 
 class StarMap extends Component {
   render() {
     return (
-      <div className="card">
-        Star Map
-        <iframe title="Map of stars" width="100%" height="400rem" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://slowe.github.io/VirtualSky/embed?&projection=polar" allowTransparency="true"></iframe>
+      <div className="card star-map">
+        <h2>Star Map (Interactive)</h2>
+
+        <div className='frame-container'>
+          <iframe
+            width="100%"
+            height="400rem"
+            frameborder="0"
+            scrolling="no"
+            marginheight="0"
+            marginwidth="0"
+            src="http://slowe.github.io/VirtualSky/embed?&projection=stereo&keyboard=false&showstarlabels=true&showdate=false&showposition=false&live=true&az=336.5"
+            allowTransparency="true" />
+        </div>
       </div>
     );
   }
