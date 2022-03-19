233bedf9806c0d36804b713e5425c82ace6bb243
James Walker <james.chunho+github@gmail.com>
2022-03-19 00:36:43 +0000

    Extract common function to helper file

---
+++ src/helpers/time.js
@@ -0,0 +1,9 @@
+// convert a timestamp like `1647650060` to a time string like `00:34`
+export function timestampToTimeString(timestamp) {
+  const date = new Date(timestamp * 1000);
+
+  const paddedHour = date.getHours().toString().padStart(2, "0");
+  const paddedMin = date.getMinutes().toString().padStart(2, "0");
+
+  return paddedHour + ":" + paddedMin;
+}
